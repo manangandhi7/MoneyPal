2c49e9b8627b26328a83cd6cea20433e8b73ca7e
Manan Gandhi <manangandhi7@gmail.com>
2017-06-08 23:01:47 +0200

    spinner added for fund transfer

--- app/src/main/java/com/MoneyPal/SendMoneyActivity.java
+++ app/src/main/java/com/MoneyPal/SendMoneyActivity.java
@@ -6,7 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import org.json.*;
 
@@ -17,16 +20,29 @@ public class SendMoneyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_money);
 
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line, FRIENDS);
+        //TODO drop down menu will be better, slect karo etle list popup thay, wither select existing one or create new one
+        Spinner textView = (Spinner)
+                findViewById(R.id.spinner);
+        AutoCompleteTextView autoCompleteTextView = (AutoCompleteTextView)
+                findViewById(R.id.receiver);
+        textView.setAdapter(adapter);
+        autoCompleteTextView.setAdapter(adapter);
+
         final Button button = (Button) findViewById(R.id.transfer_button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 //AlertDialog();
                 Log.d("hell", "how r");
-
-
             }
         });
     }
+
+    private static final String[] FRIENDS = new String[] {
+            "Add new", "Mohit", "Turi", "Kabir", "Ali", "Priya"
+    };
 
     public void selfDestruct(View view) {
         // Kabloey
