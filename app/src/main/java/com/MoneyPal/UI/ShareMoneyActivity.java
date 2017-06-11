77a65b00dab66f60aef35efe4a22efa397ed4c0c
Manan Gandhi <manangandhi7@gmail.com>
2017-06-11 18:28:04 +0200

    money sharing UI modified

--- app/src/main/java/com/MoneyPal/UI/ShareMoneyActivity.java
+++ app/src/main/java/com/MoneyPal/UI/ShareMoneyActivity.java
@@ -15,20 +15,14 @@ import com.MoneyPal.R;
 
 public class ShareMoneyActivity extends AppCompatActivity {
 
-    private LinearLayout mLayout;
-    private EditText mEditText;
-    private Button mButton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share_money);
-        mLayout = (LinearLayout) findViewById(R.id.payers);
-        //mEditText = (EditText) findViewById(R.id.editText);
-        mButton = (Button) findViewById(R.id.add_payer);
-        mButton.setOnClickListener(onClick());
-        TextView textView = new TextView(this);
-        textView.setText("New text");
+        Button button = (Button) findViewById(R.id.add_payer);
+        button.setOnClickListener(onClick());
+        button = (Button) findViewById(R.id.add_participant);
+        button.setOnClickListener(onClick());
     }
 
     private View.OnClickListener onClick() {
@@ -37,24 +31,22 @@ public class ShareMoneyActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //mLayout.addView(createNewTextView(mEditText.getText().toString()));
-                addChildLayout();
+                if(v == findViewById(R.id.add_payer)){
+                    addChildLayout(findViewById(R.id.payer_list));
+                } else if (v == findViewById(R.id.add_participant)){
+                    addChildLayout(findViewById(R.id.participant_list));
+                } else if (v == findViewById(R.id.remove_part)){
+
+                }
             }
         };
     }
 
-    private TextView createNewTextView(String text) {
-        final ActionBar.LayoutParams lparams = new ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
-        final TextView textView = new TextView(this);
-        textView.setLayoutParams(lparams);
-        textView.setText("New text: " + text);
-        return textView;
-    }
-
-    public void addChildLayout(){
+    public void addChildLayout(View v){
         //Inflater service
         LayoutInflater layoutInfralte=(LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         //parent layout xml refrence
-        LinearLayout linearLayout=(LinearLayout)findViewById(R.id.payer_list);
+        LinearLayout linearLayout=(LinearLayout) v;
         //Child layout xml refrence
         View view=layoutInfralte.inflate(R.layout.item_part_details, null);
         //add child to parent
